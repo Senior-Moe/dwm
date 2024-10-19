0f29262a41f4546093776f315ffb5626fd38e880
Senior Moe <m.kh.alnahdi@gmail.com>
2024-10-19 15:38:50 +0300

    adding colorscheme header files

---
+++ colorscheme/dark_solarized.h
@@ -0,0 +1,6 @@
+static const char col_gray1[]       = "#002b36";
+static const char col_gray2[]       = "#002b36";
+static const char col_gray3[]       = "#657b83";
+static const char col_gray4[]       = "#586e75";
+static const char col_cyan[]        = "#93a1a1";
+        
